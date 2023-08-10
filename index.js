684d3dc896834ac02db7613a423af0d0d8c5a512
Mae Delamar <maedelamar@gmail.com>
2023-08-10 12:47:56 -0500

    Fixed Navigation

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-location.href = '/';
+document.getElementById('about-btn').addEventListener('click', () => location.href = 'maedelamar.github.io/about');
+
+document.getElementById('project-btn').addEventListener('click', () => location.href = 'maedelamar.github.io/projects');
+
+const headerBtns = document.querySelectorAll('.my-stuff-btn');
+for (let btn of headerBtns) {
+    btn.addEventListener('mouseover', () => btn.style.backgroundColor = 'rgb(50, 50, 50)');
+    btn.addEventListener('mouseout', () => btn.style.backgroundColor = 'black');
+}
--- public/about/about.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('about-back-btn').addEventListener('click', () => location.href = '/');
-
-document.getElementById('project-btn').addEventListener('click', () => location.href = '/projects');
-
-const headerBtns = document.querySelectorAll('.my-stuff-btn');
-for (let btn of headerBtns) {
-    btn.addEventListener('mouseover', () => btn.style.backgroundColor = 'rgb(50, 50, 50)');
-    btn.addEventListener('mouseout', () => btn.style.backgroundColor = 'black');
-}
